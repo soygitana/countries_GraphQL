2bd53ace712f00492dd7ef5dfef961a5563c8c5f
Lizon <paula.lizon@lionbridge.com>
2021-06-30 18:53:37 +0200

    fetch data

--- countries-graphql/src/views/ContinentsList.tsx
+++ countries-graphql/src/views/ContinentsList.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 
+interface Continent {
+    name: string;
+    code: string;
+}
 
+interface ContinentsData {
+    continent: Continent[];
+}
+
+const LIST_CONTINENTS = gql`
+    {
+      continents {
+        name
+        code
+      }
+    }
+  `;
 
 const GetContinents = () => {
 
+    const { data, loading, error } = useQuery<ContinentsData>(LIST_CONTINENTS);
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    const { continents }: any = data;
 
     return (
         <>
